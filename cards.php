c375637bcca820a1ad241953f283e3f08245689b
Volodymyr Grebeniuk <grebenyukvd@gmail.com>
2023-06-14 16:12:11 +0300

    some changes

--- cards.php
+++ cards.php
@@ -26,7 +26,7 @@ $result = mysqli_query($conn,"SELECT id, card FROM cards WHERE user_id = '$useri
                                     <tr>
                                         <td> <?= $obj['id'] ?></td>
                                         <td> <?= $obj['card'] ?></td>
-                                        <td> <a href="deleted_cards.php?cardid=<?= $obj['id'] ?>">X</a></td>
+                                        <td> <a href="deleted_cards.php?cardid=<?= $obj['id'] ?>">Видалити</a></td>
                                     </tr>
                                 <?php  }?>
                             </table>
@@ -35,22 +35,19 @@ $result = mysqli_query($conn,"SELECT id, card FROM cards WHERE user_id = '$useri
                         <input type="text" class="form-control" name="card" id="card" placeholder="введіть номер картки"><br>
                         <button class="btn btn-success" type="submit">додати картку</button>
                     </form>
-                    <form action="deleted_cards.php" method="post">
-                        <input type="text" class="form-control" name="card" id="card" placeholder="введіть номер картки"><br>
-                        <button class="btn btn-success" type="submit">видалити картку</button>
-                    </form>
-<!--                        <nav class="navbar navbar-expand-sm navbar-light bg-light">-->
-<!--                            <ul class="navbar-nav">-->
-<!--                                <li class="nav-item dropdown">-->
-<!--                                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">дії</a>-->
-<!--                                    <ul class="dropdown-menu">-->
-<!--                                        <li><a class="dropdown-item" href="add_card.php">додати картку</a></li>-->
-<!--                                        <li><a class="dropdown-item" href="#">поповнити картку</a></li>-->
-<!--                                        <li><a class="dropdown-item" href="#">видалити картку</a></li>-->
-<!--                                    </ul>-->
-<!--                                </li>-->
-<!--                            </ul>-->
-<!--                        </nav>-->
+
+                        <nav class="navbar navbar-expand-sm navbar-light bg-light">
+                            <ul class="navbar-nav">
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">дії</a>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="deleted_cards.php?cardid=<?= $obj['id'] ?>">X</a></li>
+                                        <li><a class="dropdown-item" href="#">поповнити картку</a></li>
+                                        <li><a class="dropdown-item" href="#">видалити картку</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </nav>
                 </div>
             </div>
 </div>
