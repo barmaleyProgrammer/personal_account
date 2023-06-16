77ba3be2b468dd65260a30c8cf273c93e65fb72b
Sergey Soloviov <soloviov.usb@gmail.com>
2023-06-16 09:49:01 +0300

    add Docker setup

--- reg_auth/db.php
+++ reg_auth/db.php
@@ -1,8 +1,16 @@
 <?php
+if (!getenv('DB_HOST')) {
+    $envs = parse_ini_file($_SERVER['DOCUMENT_ROOT'].'/.env', false, INI_SCANNER_TYPED);
+    foreach ($envs as $key => $value) {
+        putenv("$key=$value");
+    }
+}
+
+
 $conn = mysqli_connect(
-    hostname: '127.0.0.1',
-    username: 'barmaley',
-    password: 'barmaley',
-    database: 'barmaley',
-    port: 3306
+    hostname: getenv('DB_HOST'),
+    username: getenv('DB_USER'),
+    password: getenv('DB_PASSWORD'),
+    database: getenv('DB_NAME'),
+    port: getenv('DB_PORT')
 );
