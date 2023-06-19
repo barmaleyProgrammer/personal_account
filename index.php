4acdbdbb82c0e50365889185f81c181c2da15db3
Volodymyr Grebeniuk <grebenyukvd@gmail.com>
2023-06-19 10:26:05 +0300

    test H1

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require('parts/header.php');
 <div class="container">
     <div class="col-12">
         <div id="row">
-            <h1>Реєстрація</h1>
+            <h1>Реєстраціяoiu</h1>
             <form action="reg_auth/validation-form/check.php" method="post">
                 <input type="text" class="form-control" name="login" placeholder="введіть логін" required minlength="5" maxlength="90"><br>
                 <input type="text" class="form-control" name="name" placeholder="введіть имʼя" required minlength="3" maxlength="50"><br>
