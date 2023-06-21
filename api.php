399a4396157ea48cbeb4f2a7740de7476e2d4d0e
Volodymyr Grebeniuk <grebenyukvd@gmail.com>
2023-06-21 13:15:33 +0300

    hkjh

--- api.php
+++ api.php
@@ -14,3 +14,19 @@ function seller(string $path = 'product'): array
 
     return json_decode($res, 1, 512, JSON_THROW_ON_ERROR);
 }
+
+function room(string $path = ''): array
+{
+    $ch = curl_init('https://hub2-box-asop.gerc.ua/api/v1/room/'.$path);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/json; charset=utf-8',
+        'Authorization: Token a98851dd7e7a2c6f5fb9bd6e9e25a2b8b7bb87b6'
+    ]);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    $res = curl_exec($ch);
+    curl_close($ch);
+
+    return json_decode($res, 1, 512, JSON_THROW_ON_ERROR);
+}
