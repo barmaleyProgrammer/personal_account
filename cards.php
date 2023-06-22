04a03d9623ea9bc409f9aca325b9da4548057ff2
Sergey Soloviov <soloviov.usb@gmail.com>
2023-06-22 15:28:21 +0300

    fix db connection

--- cards.php
+++ cards.php
@@ -18,7 +18,8 @@ $cards = [];
 $cards = room('card/?room='.$userid);
 foreach ($cards as $key => $card) {
     $res = room('card/'.$card['card_num'].'/balance/');
-    $cards[$key]['balance']='';
+    $numbers = array_column($res['data']['results'], 'wallet_sum');
+    $cards[$key]['balance'] = array_sum($numbers);
 }
 $del = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16"><path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/></svg>';
 ?>
@@ -31,6 +32,7 @@ $del = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="cur
                     <tr>
                         <th>nfc_id</th>
                         <th>card_num</th>
+                        <th>Баланс</th>
                         <th>blocked</th>
                         <th>archived</th>
                         <th>Видалити картку</th>
@@ -39,6 +41,7 @@ $del = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="cur
                     <tr>
                         <td><?= $card['nfc_id'] ?></td>
                         <td><?= $card['card_num'] ?></td>
+                        <td><?= $card['balance'] ?> грн</td>
                         <td><?= $card['blocked'] ?></td>
                         <td><?= $card['archived'] ?></td>
                         <td class="text-center"><a href="deleted_cards.php?cardid=<?= $card['nfc_id']; ?>"><?= $del; ?></a></td>
