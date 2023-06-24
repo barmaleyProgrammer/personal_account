bce39703bb04da099e5bcfd97472a542f7cee91c
Sergey Soloviov <soloviov.usb@gmail.com>
2023-06-24 14:18:45 +0300

    add payment api call

--- cards.php
+++ cards.php
@@ -17,9 +17,11 @@ $userid = $result->fetch_column(0);
 $cards = [];
 $cards = room('card/?room='.$userid);
 foreach ($cards as $key => $card) {
-    $res = room('card/'.$card['card_num'].'/balance/');
-    $numbers = array_column($res['data']['results'], 'wallet_sum');
-    $cards[$key]['balance'] = array_sum($numbers);
+    $balance_res = room('card/'.$card['card_num'].'/balance/');
+    $balance_nums = array_column($balance_res['data']['results'], 'wallet_sum');
+    $cards[$key]['balance'] = array_sum($balance_nums);
+
+    $cards[$key]['payments'] = room('card/'.$card['card_num'].'/payment/');
 }
 $del = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16"><path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/></svg>';
 ?>
@@ -98,4 +100,3 @@ $del = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="cur
 </div>
 <?php
 require('parts/footer.php');
-
