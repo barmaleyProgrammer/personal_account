c44f674c10f555d87f9b9b6b5eb953b9b342eb3b
Sergey Soloviov <soloviov.usb@gmail.com>
2023-07-05 18:24:12 +0300

    fix2 add_profile_data.php

--- add_profile_data.php
+++ add_profile_data.php
@@ -10,12 +10,20 @@ $comment = trim($_POST['comment']);
 $fields = [];
 if (!empty($email)) {
     $fields[] = 'email=\''.$email.'\'';
+} else {
+    $fields[] = 'email=NULL';
 }
+
 if (!empty($phone)) {
     $fields[] = 'phone=\''.$phone.'\'';
+} else {
+    $fields[] = 'phone=NULL';
 }
+
 if (!empty($comment)) {
     $fields[] = 'comments=\''.$comment.'\'';
+} else {
+    $fields[] = 'comments=NULL';
 }
 
 
