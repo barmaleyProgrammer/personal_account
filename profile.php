440aaa251f967934022cd1b07baa98b0680937a6
Volodymyr Grebeniuk <grebenyukvd@gmail.com>
2023-07-07 15:40:36 +0300

    add clearing phone num

--- profile.php
+++ profile.php
@@ -16,7 +16,12 @@ $values = mysqli_fetch_assoc($res);
         <form action="add_profile_data.php" method="post">
             <small>Ваш email:</small><input type="email" class="form-control profEmail" name="email" value="<?= $values['email']; ?>" placeholder="введіть email"><br>
 <!--            <input type="tel" pattern="[0-9]{2} [0-9]{3}[0-9]{4}" class="form-control profTel" name="phone" value="--><?php //= $values['phone']; ?><!--" placeholder="введіть номер телефону"<small>Format: 038 xxx xxxx</small><br>-->
-            <small>Формат набору номера: 38 000 000 0000</small><input onkeydown="phoneNumberFormatter()" id="phone-number" class="form-control profTel" name="phone" value="<?= $values['phone']; ?>" placeholder="введіть номер телефону"<br>
+<!--            <small>Формат набору номера: 38 000 000 0000</small><input onkeydown="phoneNumberFormatter()" onfocus="this.value=''" id="phone-number" class="form-control profTel" name="phone" value="--><?php //= $values['phone']; ?><!--" placeholder="введіть номер телефону"<br>-->
+            <div class="pass">
+            <span class="icon-pass" onclick="document.getElementById('phone-number').value = ''" title="стерти номер">&#127377;</span>
+                        <small>Формат набору номера: 38 000 000 0000</small><input onkeydown="phoneNumberFormatter()"  id="phone-number" class="form-control profTel" name="phone" value="<?= $values['phone']; ?>" placeholder="введіть номер телефону"><br>
+<!--                       <button onclick="document.getElementById('phone-number').value = ''">Очистити поле</button>-->
+           </div>
             <div class="mt-4">
                 <small>Зміст повідомлення:</small><textarea rows="10" cols="50" name="comments" ><?= $values['comments']; ?></textarea><br>
             </div>
